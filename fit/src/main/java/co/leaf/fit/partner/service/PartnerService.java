fde1c7dd0a7024a41da8f5a458c4bb863a30061f
영서 <@DESKTOP-2DEG5TM>
2021-08-23 22:40:15 +0900

    kys memPartnerSelect 8/23 10:40pm

--- fit/src/main/java/co/leaf/fit/partner/service/PartnerService.java
+++ fit/src/main/java/co/leaf/fit/partner/service/PartnerService.java
@@ -67,6 +67,7 @@ public class PartnerService implements PartnerMapper {
 	@Override
 	public List<PartnerVO> parSelectIns(PartnerVO vo) {
 		return map.parSelectIns(vo);
+	}
 
 	public int parUpdatePhoto(PartnerVO vo) {
 		return map.parUpdatePhoto(vo);
